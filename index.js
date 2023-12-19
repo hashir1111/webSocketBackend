76520f1242d37fe6c0bad8d22f04664f6e3739a5
hashir1111 <hashir6064@gmail.com>
2023-12-19 19:43:10 +0500

    changed origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000" || "http://192.168.69.107:3000",
+    origin: "https://web-socket-front-end.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
